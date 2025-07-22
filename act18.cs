bc9ff3a2dcdeb8eab2de0dddacae6e223cec4dd3
ashlyjana <ashlyjanaquising@gmail.com>
2025-07-22 12:12:15 +0800

    Update act18.cs
    Countdown Timer

--- act18.cs
+++ act18.cs
@@ -4,20 +4,22 @@ class Program
 {
     static void Main()
     {
-      Console.Write("Enter a positive number to start countdown: ");
-      string input = Console.ReadLine();
-      int number;
-      
-      bool isValid = int.TryParse(input, out number);
-      if (!isValid || number <= 0)
-      {
-        Console.WriteLine("Invalid input. Please enter a positive integer.");
-        return;
-        
+        Console.Write("Enter a positive number to start countdown: ");
+        string input = Console.ReadLine();
+        int number;
+
+        bool isValid = int.TryParse(input, out number);
+        if (!isValid || number <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a positive integer.");
+            return;
+        }
+
         Console.WriteLine("\nCountdown: ");
         while (number >= 0)
         {
-          Console.WriteLine(number);
-          number--;
+            Console.WriteLine(number);
+            number--;
+        }
     }
 }
