25b5eeec1ee456158240358c879b90493245aeb7
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-07-25 19:09:58 +0800

    Update act31.cs

--- act31.cs
+++ act31.cs
@@ -9,10 +9,8 @@ class Program
         
         bool isPalindrome = true;
 
-        // Convert to lowercase to make the comparison case-insensitive
         word = word.ToLower();
 
-        // Compare characters from start and end
         for (int i = 0; i < word.Length / 2; i++)
         {
             if (word[i] != word[word.Length - 1 - i])
